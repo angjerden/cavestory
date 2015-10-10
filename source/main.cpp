f50e03fb62fd5a6a1b1cc19f0fd663a40af32cec
Anders Gjerde <angjerden@gmail.com>
2015-10-10 23:10:50 +0200

    Main.cpp to ensure everything is set up correctly

---
+++ source/main.cpp
@@ -0,0 +1,17 @@
+/*
+ * main.cpp
+ *
+ *  Created on: 4. okt. 2015
+ *      Author: Anders
+ */
+
+#include <stdio.h>
+#include <SDL2/SDL.h>
+
+int main(int argc, char* argv[]) {
+	printf("Hi\n");
+	return 0;
+}
+
+
+
